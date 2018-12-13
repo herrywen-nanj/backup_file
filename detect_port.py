778eed638c37e54f14a23d831c197bf1af535567
herrywen-nanj <33476935+herrywen-nanj@users.noreply.github.com>
2018-12-13 15:13:58 +0800

    Update detect_port.py

--- detect_port.py
+++ detect_port.py
@@ -7,7 +7,7 @@ def detectPort():
  c = []
 #print 'a数组是' , a
  for port in a:
-     command = "lsof -i:{}".format(port)
+     command = "lsof -i:{} > /dev/null 2>&1".format(port)
     #print '你的端口分别是' , command
      if os.system(command) != 0:
         c.append(port)
