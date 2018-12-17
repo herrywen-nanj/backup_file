2c8b76eda22a2dc8b1f410ceac7ef3130a943063
herrywen <herrywen@herrywendeMacBook-Air.local>
2018-12-17 17:19:49 +0800

    update except

--- count_words.py
+++ count_words.py
@@ -2,12 +2,12 @@
 # - * - coding: utf-8 - * -
 filename = '1.txt'
 try:
-    with open(filename) as file_object:
-      t = file_object.read()
-except FileNotFoundError:
-    print 'you have' + filename + "is not exist!"
+ with open(filename) as file_object:
+  t = file_object.read()
+except IOError:
+ print 'you have' + ' ' + filename + " is not exist!"
 else:
-    words = t.split()
+ words = t.split()
     #print的两种写法，可以带逗号，后面直接跟参数值。也可以不带逗号，后面直接跟%参数值
-    print 'danci de geshi yigong shi %d'  %len(words)
-    #print 'danci de geshi yigong shi' , len(words)
+ print 'danci de geshi yigong shi %d'  %len(words)
+#    #print 'danci de geshi yigong shi' , len(words)
