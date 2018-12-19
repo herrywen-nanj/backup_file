8c6f4d21e0400052414e4f4ae5f7568fd3b02d25
herrywen <herrywen@herrywendeMacBook-Air.local>
2018-12-19 14:15:46 +0800

    添加写周报脚本

---
+++ weekday_report.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# - * - coding: utf-8 - * -
+#作用：写周报
+import datetime
+filename = '1.txt'
+string_date = '%Y年-%m月-%d日'
+weekdate = []
+raw_content = []
+def getYesterday(): 
+    today=datetime.date.today()
+    #weekdate.append(today)
+    for i in range(0,5):
+     oneday=datetime.timedelta(days=i)
+     yesterday=today-oneday
+     yesterday = yesterday.strftime(string_date)  
+     weekdate.append(yesterday)
+
+getYesterday()
+weekdate.reverse()
+
+
+def get_rawcontent():
+ j = 0
+ while j< 5:
+  content = raw_input("please input you content: ")
+  raw_content.append(content)
+  j += 1
+get_rawcontent()
+
+def get_report():
+ for i in range(0,5):
+   count_report = weekdate[i] + ' ' + raw_content[i] + "\n"
+   with open(filename,'a') as file_object:
+     file_object.write(count_report)
+get_report()
+
+
+
+
+
