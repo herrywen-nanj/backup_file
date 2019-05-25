84e5c3f4dc4ef809a118f6d3519d4a756f62df6c
herrywen-nanj <33476935+herrywen-nanj@users.noreply.github.com>
2019-05-25 16:27:21 +0800

    Create find_max_and_change_postion.py

---
+++ find_max_and_change_postion.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+list1 = [5, 12, 32, 4, 2, 10, 25]
+count = 0
+for i in list1:
+    count += 1
+    n = len(list1)
+    if i == max(list1):
+            #a = list1[count-1]
+            #b = list1[n-1]
+            #list1[count-1] = b
+            #list1[n-1] = a
+        list1[count-1],list1[n-1] = list1[n-1],list1[count-1]
+print(list1)
+
+
+输出结果:
+[5, 12, 25, 4, 2, 10, 32]
